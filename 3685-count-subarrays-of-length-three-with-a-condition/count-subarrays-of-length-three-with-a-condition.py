b73aae78c939e0b4d2c0543ed8e7511b32c2d867
SornamThiyagarajan <36667578+SornamThiyagarajan@users.noreply.github.com>
2025-04-27 21:26:07 +0530

    Time: 7 ms (58.57%) | Memory: 18 MB (7.35%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
@@ -3,18 +3,11 @@ class Solution:
         count = 0 
         output_arr = []
         for each in range(1, len(nums)-1):
-            #print(nums[each])
+            
             curr = nums[each]
             prev = nums[each-1]
             nxt = nums[each+1]
-            #print("\nCurrent Element:",nums[each])
-            #print("Prev Element:",nums[each-1])
-            #print("Next Element:",nums[each+1])           
-            #print("Sum:",prev+nxt)
-            #print("Condn:",curr/2)
-            #print("***")
-            if (prev+nxt) == (curr/2):
-                #print("Pass")
+            if (prev+nxt) == (curr/2):            
                 count = count+1
                 output_arr.append(prev)
                 output_arr.append(curr)
