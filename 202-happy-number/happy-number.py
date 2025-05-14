3fe375995aaa9895c5ce9b8d84d13c1f6e189735
SornamThiyagarajan <36667578+SornamThiyagarajan@users.noreply.github.com>
2025-05-14 12:02:29 +0530

    Time: 3 ms (41.90%) | Memory: 17.7 MB (86.86%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def sum_of_squares(num):
+            return sum(int(digit)** 2 for digit in str(num))
+        seen = set() 
+        while n!=1 and n not in seen:
+            seen.add(n)
+            n = sum_of_squares(n)
+        return n == 1 
