36d2f78c87b2a8afd23cf38a1eb7ad9c626b2fc5
SornamThiyagarajan <36667578+SornamThiyagarajan@users.noreply.github.com>
2025-06-10 02:31:00 +0530

    Time: 4 ms (24.77%) | Memory: 17.8 MB (82.47%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,13 +1,3 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        '''
-        ans = []
-        inp_idx = [(x,nums[x]) for x in range(len(nums))]      
-        for i in range(0,len(nums)):               
-            idx_temp = nums[i]
-            ans.append(inp_idx[idx_temp][1])           
-        return ans'''
-
-        #optimized version
         return [nums[nums[i]] for i in range(len(nums))]
-        
