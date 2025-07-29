f92bd8073cad4e8c139686375cda41bd344a31d6
SornamThiyagarajan <36667578+SornamThiyagarajan@users.noreply.github.com>
2025-07-29 19:10:40 +0200

    Time: 0 ms (100.00%) | Memory: 17.7 MB (79.38%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -3,12 +3,10 @@ class Solution:
         n = len(digits)
     
         for i in range(n - 1, -1, -1):
-            # If the digit is less than 9, just add one and return
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
-            # Otherwise, set current digit to 0 and continue
-            digits[i] = 0
-        
-        # If we exited the loop, all digits were 9 (e.g., 999 → 1000)
-        return [1] + [0] * n
+            digits[i] = 0  # Set current to 0 and carry will continue
+
+        # If all were 9s, then we need to add 1 at the front
+        return [1] + digits
