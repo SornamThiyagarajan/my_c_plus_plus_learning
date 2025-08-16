9970bbda34e1968adcc6f0fc357db684f688d267
SornamThiyagarajan <36667578+SornamThiyagarajan@users.noreply.github.com>
2025-08-16 21:09:13 +0200

    Time: 0 ms (100.00%) | Memory: 7.8 MB (93.52%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.c
@@ -0,0 +1,14 @@
+int maximum69Number (int num) {
+    char str_num[6]; // As per num range constraint restriction num can hold upto 10^4 which is equivalent of 5+1 (6 char including /0)
+    sprintf(str_num, "%d",num); //convert num to string 
+    for(int i = 0; i < strlen(str_num); i++)
+     { 
+        //printf("\ni: %d , ch: %c", i, str_num[i]);
+        if(str_num[i] == '6'){
+            str_num[i] = '9';
+            break;
+        }
+     }
+     num = atoi(str_num);
+     return num;
+}
