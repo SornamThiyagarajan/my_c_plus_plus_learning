164e8827bfc367a9896610cad809953c0ea6433c
SornamThiyagarajan <36667578+SornamThiyagarajan@users.noreply.github.com>
2025-09-24 20:04:07 +0200

    Time: 3 ms (36.04%) | Memory: 18 MB (35.42%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
@@ -1,14 +1,8 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-        # Put broken letters into a set for quick lookup
         broken = set(brokenLetters)
-        
-        # Split text into words
-        words = text.split()
-        
         count = 0
-        for word in words:
-            # If no broken letter is in the word, it's valid
-            if not (set(word) & broken):  # intersection check
+        for word in text.split():
+            if not any(ch in broken for ch in word):
                 count += 1
         return count
